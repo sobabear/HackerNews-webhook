031db1cb948b1069e28ea403d448ab8fbff9a4ff
Yongjun <stareta1202@gmail.com>
2023-10-29 23:16:03 +0900

    bug fixs

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send(`get Hello ${req.query.name || req.body.name || 'World'}!`);
+    console.log(`wow`);
+    selectAndPost();
+    res.send(`get Hello World'}!`);
 });
 
 app.post('/', (req, res) => {
+    console.log(`wow2`);
     selectAndPost();
-    res.send(`Post Hello ${req.query.name || req.body.name || 'World'}!`);
+    res.send(`Post Hello World'}!`);
 });
 
-app.listen(port, () => {
-    console.log(`Rest API started successfully on port ${port}`);
-});
+
 
 
 var CONFIG = {
-    "webhook_url": "https://hooks.slack.com/services/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",
+    "webhook_url": "https://hooks.slack.com/services/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",
     "channel": "#general",
     "bot_username": "HackerNewsBot",
     "bot_icon_emoji": ":newspaper:",
@@ -47,6 +48,7 @@ function selectAndPost() {
                 selectAndPost();
             }
         } else {
+            console.log('5252');
             body = JSON.parse(body);
             var linkID = body[Math.floor(Math.random() * body.length)];
             var url = "https://hacker-news.firebaseio.com/v0/item/" + linkID + ".json";
@@ -65,26 +67,29 @@ function selectAndPost() {
 
 function postLink(title, link, score, author) {
 
-    // Construct the call to the Slack webhoook using the scraped link and configuration options.
+    // // Construct the call to the Slack webhoook using the scraped link and configuration options.
+    // var data = {
+    //     "channel": CONFIG.channel,
+    //     "username": CONFIG.bot_username,
+    //     "icon_emoji": CONFIG.bot_icon_emoji,
+    //     "attachments": [{
+    //         "title": title,
+    //         "title_link": link,
+    //         "fallback": CONFIG.fallback,
+    //         "color": CONFIG.post_color,
+    //         "fields": [{
+    //             "title": "Author",
+    //             "value": author,
+    //             "short": true
+    //         }, {
+    //             "title": "Score",
+    //             "value": score,
+    //             "short": true
+    //         }]
+    //     }]
+    // };
     var data = {
-        "channel": CONFIG.channel,
-        "username": CONFIG.bot_username,
-        "icon_emoji": CONFIG.bot_icon_emoji,
-        "attachments": [{
-            "title": title,
-            "title_link": link,
-            "fallback": CONFIG.fallback,
-            "color": CONFIG.post_color,
-            "fields": [{
-                "title": "Author",
-                "value": author,
-                "short": true
-            }, {
-                "title": "Score",
-                "value": score,
-                "short": true
-            }]
-        }]
+        "text": title
     };
 
     var options = {
@@ -94,13 +99,21 @@ function postLink(title, link, score, author) {
         method: "post"
     };
 
-    request(options, function cb(err, httpResponse, body) {
-        if (err) {
-            console.error("Newsbot failed to post to Slack! Message: " + JSON.stringify(body));
-        }
-    });
+    console.log(title);
+
+    request.post(
+        {
+          headers : { 'Content-type' : 'application/json' },
+          url: CONFIG.webhook_url,
+          form : {payload: JSON.stringify(data )}
+        },
+        (error, res, body) => console.log(error, body, res.statusCode)
+      );
 }
 
+app.listen(port, () => {
+    console.log(`Rest API started successfully on port ${port}`);
+});
 
 functions.http('selectAndPost', (req, res) => {
   res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
