f58508d60cbed14c833380d6c2ee3f381aecec0d
brunel.lee <brunel.lee@kakaohealthcare.com>
2023-10-30 11:03:03 +0900

    Fix: top stories 10개로 변경

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ app.post('/', (req, res) => {
     res.send(`Post Hello World'}!`);
 });
 
+var webhook_url = "";
 
 var CONFIG = {
-    "webhook_url": "https://hooks.slack.com/services/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",
+    "webhook_url": webhook_url,
     "channel": "#general",
     "bot_username": "HackerNewsBot",
     "bot_icon_emoji": ":newspaper:",
@@ -27,62 +28,40 @@ var CONFIG = {
 };
 
 // selectAndPost();
+// var api_url = ["https://hacker-news.firebaseio.com/v0/topstories.json", "https://hacker-news.firebaseio.com/v0/newstories.json", "https://hacker-news.firebaseio.com/v0/showstories.json", "https://hacker-news.firebaseio.com/v0/topstories.json"];
+var topStories = "https://hacker-news.firebaseio.com/v0/topstories.json";
+// var newStories = "https://hacker-news.firebaseio.com/v0/newstories.json";
+// var showStories = "https://hacker-news.firebaseio.com/v0/showstories.json";
 
 function selectAndPost() {
-    // Choose a random URL pool from HN, with greater probability of selecting top stories. 
-    var api_url = ["https://hacker-news.firebaseio.com/v0/topstories.json", "https://hacker-news.firebaseio.com/v0/newstories.json", "https://hacker-news.firebaseio.com/v0/showstories.json", "https://hacker-news.firebaseio.com/v0/topstories.json"];
-    var api_url = api_url[Math.floor(Math.random() * api_url.length)];
-
     request({
-        url: api_url
+        url: topStories
     }, function cb(err, httpResponse, body) {
-
         if (err) {
-            console.error("Newsbot failed to fetch " + api_url + "!");
-            // Retry if set in config. 
+            console.error("Newsbot failed to fetch " + topStories + "!");
             if (CONFIG.retry) {
                 selectAndPost();
             }
         } else {
             body = JSON.parse(body);
-            var linkID = body[Math.floor(Math.random() * body.length)];
-            var url = "https://hacker-news.firebaseio.com/v0/item/" + linkID + ".json";
-
-            request(url, function optionalCallback(err, httpResponse, body) {
-                if (err) {
-                    console.error("Hacker News Scraper failed to fetch " + url + "!");
-                } else {
-                    body = JSON.parse(body);
-                    postLink(body.title, body.url, body.score, body.by);
-                }
-            });
+            var index;
+            for (index = 0; index < 10; index++) {
+                var linkID = body[index];
+                var url = "https://hacker-news.firebaseio.com/v0/item/" + linkID + ".json";
+                request(url, function optionalCallback(err, httpResponse, body) {
+                    if (err) {
+                        console.error("Hacker News Scraper failed to fetch " + url + "!");
+                    } else {
+                        body = JSON.parse(body);
+                        postLink(body.title, body.url, body.score, body.by);
+                    }
+                });
+            }
         }
     });
 }
 
 function postLink(title, link, score, author) {
-
-    // // Construct the call to the Slack webhoook using the scraped link and configuration options.
-    // var data = {
-    //     "channel": CONFIG.channel,
-    //     "username": CONFIG.bot_username,
-    //     "icon_emoji": CONFIG.bot_icon_emoji,
-    //     "attachments": [{
-    //         "title": title,
-    //         "title_link": link,
-    //         "fallback": CONFIG.fallback,
-    //         "color": CONFIG.post_color,
-    //         "fields": [{
-    //             "title": "Author",
-    //             "value": author,
-    //             "short": true
-    //         }, {
-    //             "title": "Score",
-    //             "value": score,
-    //             "short": true
-    //         }]
-    //     }]
-    // };
     var data = {
         "text": title
     };
